dadda011f6ce23749e55c33fdaa99a65bd3ec569
peng <1110@forex.com.cn>
2020-05-11 10:43:43 +0800

    optimization

--- update-dx-web.sh
+++ update-dx-web.sh
@@ -7,5 +7,10 @@ git pull
 echo 'cd shell/dx-web/'
 cd shell/dx-web/
 
+# 编译代码
 echo 'mvn clean install --settings ~/.m2/settings-new-work.xml -Dmaven.test.skip=true'
 mvn clean deploy --settings ~/.m2/settings-new-work.xml -Dmaven.test.skip=true -T6 -Penv_staging
+
+# 将产物推送到目标服务路径
+
+# 重启服务
