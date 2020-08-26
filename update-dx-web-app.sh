0b1abe18a5471bc54c20d853eb3921f9f32bbbe1
peng <gpqhl0071@126.com>
2020-08-26 14:48:36 +0800

    code optimization

--- update-dx-web-app.sh
+++ update-dx-web-app.sh
@@ -1,11 +1,14 @@
-echo 'checkout $0'
+# 更新分支代码
+echo 'checkout $1'
 cd ../dx-web-app/
 git checkout .
-git checkout "$0"
+git checkout "$1"
 git pull
 
+# 写入deploy相关配置
 cd ../maven-dx-shell/
 sed -i '/<\/build>/r deploy.xml' ../dx-web-app/pom.xml
 
+# 构建项目
 cd ../dx-web-app/
 /home/dx_write/apache-maven-3.6.3/bin/mvn clean deploy --settings /usr/share/maven/conf/settings-new-work.xml -Dmaven.test.skip=true -T6 -Penv_staging
