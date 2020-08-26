0cbba8c9865bab73dca774ce0a203a260e7e252b
peng <gpqhl0071@126.com>
2020-08-26 14:37:51 +0800

    code optimization

--- update-dx-web-app.sh
+++ update-dx-web-app.sh
@@ -8,4 +8,4 @@ cd ../maven-dx-shell/
 sed -i '/<\/build>/r deploy.xml' ../dx-web-app/pom.xml
 
 cd ../dx-web-app/
-mvn clean deploy -P env_staging --settings /usr/share/maven/conf/settings-new-work.xml -Dmaven.test.skip=true -T6 -Penv_staging
+mvn clean deploy --settings /usr/share/maven/conf/settings-new-work.xml -Dmaven.test.skip=true -T6 -Penv_staging
