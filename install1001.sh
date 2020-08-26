a34200ba71b957d7e9390331a8f807f516bb6dbe
peng <gpqhl0071@126.com>
2020-08-26 17:39:55 +0800

    code optimization

--- install1001.sh
+++ install1001.sh
@@ -34,6 +34,7 @@ cd ../dx-web-app/
 cd ../dx-web/
 /home/dx_write/apache-maven-3.6.3/bin/mvn clean deploy -P env_staging --settings /usr/share/maven/conf/settings-new-work.xml -Dmaven.test.skip=true -T6
 
+cd ../maven-dx-shell/
 for i in "$@"; do
   sh restart1001.sh "$i"
 done
