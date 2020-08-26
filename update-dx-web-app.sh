f2d6f59c02cdb19af6c3f402faf2be505e3a7c31
peng <gpqhl0071@126.com>
2020-08-26 14:35:25 +0800

    code optimization

--- update-dx-web-app.sh
+++ update-dx-web-app.sh
@@ -7,4 +7,4 @@ git pull
 cd ../maven-dx-shell/
 sed -i '/<\/build>/r deploy.xml' ../dx-web-app/pom.xml
 
-mvn clean deploy -P env_staging --settings ~/.m2/settings-new-work.xml -Dmaven.test.skip=true -T6 -Penv_staging
+mvn clean deploy -P env_staging --settings /usr/share/maven/conf/settings-new-work.xml -Dmaven.test.skip=true -T6 -Penv_staging
--- update-dx-web.sh
+++ update-dx-web.sh
@@ -9,7 +9,7 @@ cd shell/dx-web/
 
 # 编译代码
 echo 'mvn clean install --settings ~/.m2/settings-new-work.xml -Dmaven.test.skip=true'
-mvn clean deploy --settings ~/.m2/settings-new-work.xml -Dmaven.test.skip=true -T6 -Penv_staging
+mvn clean deploy --settings /usr/share/maven/conf/settings-new-work.xml -Dmaven.test.skip=true -T6 -Penv_staging
 
 # 将产物推送到目标服务路径
 
