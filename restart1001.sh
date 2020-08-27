68ec6ea57eba9a0e014f7b6ca4a703ff5015b99d
peng <gpqhl0071@126.com>
2020-08-27 17:36:05 +0800

    code optimization

--- restart1001.sh
+++ restart1001.sh
@@ -48,9 +48,8 @@ eeooff
 
   # 构建远程服务，重启tomcat
   ssh dx@"$ip" >/dev/null 2>&1 <<eeooff
-unzip -o /www/webapp/"$project_name"/work/*.war
-echo unzip -o /www/webapp/"$project_name"/work/*.war
 cd /www/webapp/
+unzip -o /www/webapp/"$project_name"/work/*.war
 sh stop.sh "$tomcat_name"
 sh dxrestart.sh "$tomcat_name"
 exit
