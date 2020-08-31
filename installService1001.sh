63ac7d4ad296f3b35315869df4e792920ff3ad34
peng <gpqhl0071@126.com>
2020-08-31 10:08:07 +0800

    code optimization

---
+++ installService1001.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+root_path="/home/dx_write/project/"
+
+if [ "$1" == "" ]; then
+  echo branch is null
+  exit 1
+elif [ "$2" == "" ]; then
+  echo project_name is null
+  exit 1
+fi
+
+# 更新分支代码
+echo 'checkout $1'
+cd ../dx-service/
+git checkout .
+git checkout "$1"
+git pull
+
+# 写入deploy相关配置
+cd "${root_path}"/maven-dx-shell/
+sed -i '/<\/build>/r deploy.xml' ../dx-service/pom.xml
+
+# 构建项目
+cd "${root_path}"/dx-service/
+/home/dx_write/apache-maven-3.6.3/bin/mvn clean deploy -P env_staging --settings /usr/share/maven/conf/settings-new-work.xml -Dmaven.test.skip=true -T6
+
+cd "${root_path}"/maven-dx-shell/
+for i in "$@"; do
+  sh restartService1001.sh "$i"
+done
---
+++ restartService1001.sh
@@ -0,0 +1,45 @@
+i="$1"
+project_name=""
+ip="192.168.15.32"
+local_project_name="dx-service"
+
+if [ $i == "dx-public-service" ]; then
+  echo "dx-public-service"
+  project_name="dx-public-service"
+elif [ $i == "dx-ctivity-service" ]; then
+  echo "dx-ctivity-service"
+  project_name="dx-ctivity-service"
+elif [ $i == "dx-strategy-service" ]; then
+  echo "dx-strategy-service"
+  project_name="dx-strategy-service"
+elif [ $i == "dx-user-service" ]; then
+  echo "dx-user-service"
+  project_name="dx-user-service"
+elif [ $i == "dx-assets-service" ]; then
+  echo "dx-assets-service"
+  project_name="dx-assets-service"
+else
+  echo "没有符合的条件"
+fi
+
+if [ "$project_name" != "" ]; then
+  # 远程传输,删除服务器端对应的升级包
+  ssh dx@"$ip" >/dev/null 2>&1 <<eeooff
+cd /www/webapp/"$project_name"/work/
+rm -rf *.tar.gz
+exit
+eeooff
+  echo done!
+
+  scp /home/dx_write/project/"$local_project_name"/target/*.tar.gz dx@"$ip":/www/webapp/"$project_name"/work/
+
+  # 构建远程服务，重启tomcat
+  ssh dx@"$ip" >/dev/null 2>&1 <<eeooff
+cd /www/webapp/"$project_name"/work/
+tar -zxvf /www/webapp/"$project_name"/work/*.tar.gz
+cd /www/webapp/"$project_name"/work/bin
+sh restart.sh
+exit
+eeooff
+  echo done!
+fi
