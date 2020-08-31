57ecde68e1811b427904e36beaa29817f8d52fef
peng <gpqhl0071@126.com>
2020-08-31 10:19:36 +0800

    code optimization

--- restartService1001.sh
+++ restartService1001.sh
@@ -2,22 +2,34 @@ i="$1"
 project_name=""
 ip="192.168.15.32"
 local_project_name="dx-service"
+local_project_name_1="dx-public"
+local_project_name_2="dx-public-api-service"
 
 if [ $i == "dx-public-service" ]; then
   echo "dx-public-service"
   project_name="dx-public-service"
+  local_project_name_1="dx-public"
+  local_project_name_2="dx-public-api-service"
 elif [ $i == "dx-ctivity-service" ]; then
   echo "dx-ctivity-service"
   project_name="dx-ctivity-service"
+  local_project_name_1="dx-ctivity"
+  local_project_name_2="dx-ctivity-api-service"
 elif [ $i == "dx-strategy-service" ]; then
   echo "dx-strategy-service"
   project_name="dx-strategy-service"
+  local_project_name_1="dx-strategy"
+  local_project_name_2="dx-strategy-api-service"
 elif [ $i == "dx-user-service" ]; then
   echo "dx-user-service"
   project_name="dx-user-service"
+  local_project_name_1="dx-user"
+  local_project_name_2="dx-user-api-service"
 elif [ $i == "dx-assets-service" ]; then
   echo "dx-assets-service"
   project_name="dx-assets-service"
+  local_project_name_1="dx-assets"
+  local_project_name_2="dx-assets-api-service"
 else
   echo "没有符合的条件"
 fi
@@ -31,7 +43,7 @@ exit
 eeooff
   echo done!
 
-  scp /home/dx_write/project/"$local_project_name"/"${project_name}"/target/*.tar.gz dx@"$ip":/www/webapp/"$project_name"/work/
+  scp /home/dx_write/project/"$local_project_name"/"${local_project_name_1}"/${local_project_name_2}/target/*.tar.gz dx@"$ip":/www/webapp/"$project_name"/work/
 
   # 构建远程服务，重启tomcat
   ssh dx@"$ip" >/dev/null 2>&1 <<eeooff
