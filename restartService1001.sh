c1ecb9a8f4d7de2d40e3c15741f3148f3ba01a90
peng <gpqhl0071@126.com>
2020-09-01 16:15:40 +0800

    代码优化

--- restartService1001.sh
+++ restartService1001.sh
@@ -51,8 +51,7 @@ cd /www/webapp/"$project_name"/
 tar -zxvf /www/webapp/"$project_name"/*.tar.gz
 file_name=$(ls *.tar.gz)
 file_name_d=${file_name%-assembly.tar.gz}
-rm -rf work/bin work/conf/ work/lib/
-cp -r /www/webapp/"$project_name"/"$file_name_d"/* /www/webapp/"$project_name"/work/
+
 cd /www/webapp/"$project_name"/work/bin
 sh restart.sh
 exit
