8a6c1060c4ac93e504148518de42e650c9585052
peng <gpqhl0071@126.com>
2020-09-07 17:25:53 +0800

    code optimization

--- restart1001.sh
+++ restart1001.sh
@@ -31,30 +31,30 @@ else
   echo "没有符合的条件"
 fi
 
-if [ "$project_name" != "" ]; then
+if [ "${project_name}" != "" ]; then
   # 远程传输,删除服务器端对应的升级包
   ssh dx@"$ip" >/dev/null 2>&1 <<eeooff
-cd /www/webapp/"$project_name"/work/
+cd /www/webapp/"${project_name}"/work/
 rm -rf *.war
 exit
 eeooff
   echo done!
 
   if [ "$local_project_name" == "dx-web" ]; then
-    scp /home/dx_write/project/"$local_project_name"/target/*.war dx@"$ip":/www/webapp/"$project_name"/work/
+    scp /home/dx_write/project/"$local_project_name"/target/*.war dx@"$ip":/www/webapp/"${project_name}"/work/
   else
-    scp /home/dx_write/project/"$local_project_name"/"$project_name"/target/*.war dx@"$ip":/www/webapp/"$project_name"/work/
+    scp /home/dx_write/project/"$local_project_name"/"${project_name}"/target/*.war dx@"$ip":/www/webapp/"${project_name}"/work/
   fi
 
   # 构建远程服务，重启tomcat
   ssh dx@"$ip" >/dev/null 2>&1 <<eeooff
-cd /www/webapp/"$project_name"/work/WEB-INF/lib/
+cd /www/webapp/"${project_name}"/work/WEB-INF/lib/
 rm -rf *.jar
-cd /www/webapp/"$project_name"/work/
-unzip -o /www/webapp/"$project_name"/work/*.war
+cd /www/webapp/"${project_name}"/work/
+unzip -o /www/webapp/"${project_name}"/work/*.war
 cd /www/webapp/
-sh stop.sh "$tomcat_name"
-sh dxrestart.sh "$tomcat_name"
+sh stop.sh "${tomcat_name}"
+sh dxrestart.sh "${tomcat_name}"
 exit
 eeooff
   echo done!
