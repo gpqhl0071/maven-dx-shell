becf16d02b498b26334b6ecd142d3d3a13bffca8
peng <gpqhl0071@126.com>
2020-09-15 09:54:51 +0800

    code optimization

--- 1001bin/stop.sh
+++ 1001bin/stop.sh
@@ -1,7 +1,15 @@
-pid=$(ps -ef | grep "$1" | grep -v grep | awk '{print $2}')
-echo $pid
-#echo ---------
-kill -9 $pid
-#echo ===========
-#echo sh /www/"$1"/bin/startup.sh
-#sh /www/"$1"/bin/startup.sh
+appname="$1"
+PID=`ps aux | grep $appname | grep -v grep | awk '{print $2}'`
+if [ -n "$PID" ]; then
+
+       echo "$PID";
+       echo "==== start Killing app $1 ====";
+       sleep 1;
+       ps aux | grep $appname | grep -v grep | awk '{print $2}'|xargs kill -9;
+       echo "app: $1 killed";
+
+else
+
+       echo "==== app $1 not running ====";
+       exit 1;
+fi
