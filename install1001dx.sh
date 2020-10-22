902e18e5544bbbc4cea2c6d026c3806676763868
peng <gpqhl0071@126.com>
2020-10-22 10:29:42 +0800

    update config

--- install1001dx.sh
+++ install1001dx.sh
@@ -38,12 +38,12 @@ fi
 if [ ${is_dx_web} == "0" ]; then
   # 写入deploy相关配置
   cd "${root_path}"/maven-dx-shell/
-  sed -i '/<\/build>/r deploy.xml' ../dx-web-app/pom.xml
+  sed -i '/<\/build>/r deploy.xml' "${root_path}"/gs/dx-web-app/pom.xml
 fi
 
 if [ ${is_dx_web} == "1" ]; then
   cd "${root_path}"/maven-dx-shell/
-  sed -i '/<\/build>/r deploy.xml' ../dx-web/pom.xml
+  sed -i '/<\/build>/r deploy.xml' "${root_path}"/gs/dx-web/pom.xml
 fi
 
 if [ ${is_dx_web} == "0" ]; then
