8ff468242b7471c3f984be4063427aaaf5f6de76
peng <gpqhl0071@126.com>
2020-11-25 17:28:24 +0800

    add gs aps

---
+++ install1001H5_gs.sh
@@ -0,0 +1,14 @@
+
+cd /home/dx_write/project/gs/dx-aps-h5/
+
+mvn clean package -P test --settings /usr/share/maven/conf/settings-new-work.xml -Dmaven.test.skip=true
+
+cd target/
+
+cp dx-aps-h5-6.2.0-SNAPSHOT.jar /home/dx_write/dx-aps-h5/
+
+mv dx-aps-h5-6.2.0-SNAPSHOT.jar dx-aps-h5.jar
+
+sh restart.sh
+
+echo "success"
