507f2ec192cc732ef6b71dc6fc57aed2de2fa561
peng <gpqhl0071@126.com>
2020-11-26 11:20:38 +0800

    增加scp传输脚本

--- install1001H5_gs.sh
+++ install1001H5_gs.sh
@@ -1,4 +1,6 @@
 
+sh notice.sh "公社 ：【后端服务接口】，正在部署重启服务..."
+
 cd /home/dx_write/project/gs/dx-aps-h5/
 
 mvn clean package -P test --settings /usr/share/maven/conf/settings-new-work.xml -Dmaven.test.skip=true
