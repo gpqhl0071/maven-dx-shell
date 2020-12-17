f022769c69916b376d3298ae2241dd6fe257ed7b
peng <gpqhl0071@126.com>
2020-12-17 08:33:27 +0800

    增加国际H5脚本

--- install1001H5.sh
+++ install1001H5.sh
@@ -3,9 +3,11 @@ branch_name="$1"
 
 mv /home/dx_write/repository/com/redhorse/dx-common/${branch_name}/ /home/dx_write/repository/com/redhorse/dx-common/${branch_name}_$(date +%Y%m%d_%H%M%S_%N)
 
-sh notice.sh "国际 ：【后端服务接口】，正在部署重启服务..."
+sh notice.sh "国际H5 ：【后端服务接口】，正在部署重启服务..."
 
-cd /home/dx_write/project/gs/dx-aps-h5-1001co/
+cd /home/dx_write/project/dx-aps-h5/
+
+git checkout ${branch_name}
 
 git pull
 
@@ -19,7 +21,7 @@ cd /home/dx_write/dx-aps-h5-1001co/
 
 mv dx-aps-h5-${branch_name}.jar dx-aps-h5-1001co.jar
 
-sh notice.sh "国际 ：【后端服务接口】，完成部署。"
+sh notice.sh "国际H5 ：【后端服务接口】，完成部署。"
 
 sh restart.sh
 echo "success"
