22d1063aec5f177391bb033562b289f583405585
peng <gpqhl0071@126.com>
2020-12-17 08:30:06 +0800

    增加国际H5脚本

---
+++ install1001H5.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+branch_name="$1"
+
+mv /home/dx_write/repository/com/redhorse/dx-common/${branch_name}/ /home/dx_write/repository/com/redhorse/dx-common/${branch_name}_$(date +%Y%m%d_%H%M%S_%N)
+
+sh notice.sh "国际 ：【后端服务接口】，正在部署重启服务..."
+
+cd /home/dx_write/project/gs/dx-aps-h5-1001co/
+
+git pull
+
+mvn clean package -P test --settings /usr/share/maven/conf/settings-new-work.xml -Dmaven.test.skip=true
+
+cd target/
+
+cp dx-aps-h5-${branch_name}.jar /home/dx_write/dx-aps-h5-1001co/
+
+cd /home/dx_write/dx-aps-h5-1001co/
+
+mv dx-aps-h5-${branch_name}.jar dx-aps-h5-1001co.jar
+
+sh notice.sh "国际 ：【后端服务接口】，完成部署。"
+
+sh restart.sh
+echo "success"
