bde0857cc0a88141149345c56dc74eac560bb98c
peng <gpqhl0071@126.com>
2021-03-18 17:14:38 +0800

    update aps shell

--- install1001H5.sh
+++ install1001H5.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-branch_name="$1"
+version="$1"
 remark="$2"
 
-mv /home/dx_write/repository/com/redhorse/dx-common/${branch_name}/ /home/dx_write/repository/com/redhorse/dx-common/${branch_name}_$(date +%Y%m%d_%H%M%S_%N)
+mv /home/dx_write/repository/com/redhorse/dx-common/feature-${version}/ /home/dx_write/repository/com/redhorse/dx-common/feature-${version}_$(date +%Y%m%d_%H%M%S_%N)
 
-sh notice.sh "国际H5【${branch_name}】【后端服务接口】，正在部署重启服务...部署内容【${remark}】"
+sh notice.sh "国际H5【${version}】【后端服务接口】，正在部署重启服务...部署内容【${remark}】"
 
 cd /home/dx_write/project/dx-aps-h5/
 
-git checkout ${branch_name}
+git checkout feature-${version}
 
 git pull
 
@@ -16,13 +16,13 @@ mvn clean package -P test --settings /usr/share/maven/conf/settings-new-work.xml
 
 cd target/
 
-cp dx-aps-h5-${branch_name}.jar /home/dx_write/dx-aps-h5-1001co/
+cp dx-aps-h5-${version}-SNAPSHOT.jar /home/dx_write/dx-aps-h5-1001co/
 
 cd /home/dx_write/dx-aps-h5-1001co/
 
-mv dx-aps-h5-${branch_name}.jar dx-aps-h5-1001co.jar
+mv dx-aps-h5-${version}-SNAPSHOT.jar dx-aps-h5-1001co.jar
 
-sh /home/dx_write/project/maven-dx-shell/notice.sh "国际H5【${branch_name}】【后端服务接口】，完成部署。"
+sh /home/dx_write/project/maven-dx-shell/notice.sh "国际H5【${version}】【后端服务接口】，完成部署。"
 
 sh restart.sh
 echo "success"
